ce9617fb8e5b8565f937d5271754252db3015e45
UrquizaN <urquiza-nobrega@outlook.com>
2020-07-26 17:59:03 -0300

    added assets

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -43,7 +43,7 @@ class PointsController {
         */
        const serializedPoint = {
             ...point,
-            image_url: `http://192.168.0.3:3333/uploads/${point.image}`,
+            image_url: `http://192.168.0.5:3333/uploads/${point.image}`,
         };
     
         const items = await knex('items')
